3d238e03c325bf149575d3a10063f1352859161e
Maxim Vainshtein <maximv88@gmail.com>
2016-03-27 20:59:30 +0300

    Initial Commit

---
+++ Ex1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Ex1
+//
+//  Created by Maxim Vainshtein on 27/03/2016.
+//  Copyright © 2016 Maxim Vainshtein. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
